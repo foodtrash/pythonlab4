386ac61b68e4d746748e430e8fc836f63505580e
h4mmilton <rmncsgo@gmail.com>
2020-04-09 13:26:57 +0600

    reconstructed to Class

--- pythonlab3/AandB.py
+++ pythonlab3/AandB.py
@@ -1,35 +1,29 @@
-
-def numbers(a,b):
-  array=[0]*(b-a)
-  iterator=0
-  if a==b:
-    array.append(a)
+class AandB():
+  def __init__(self,a,b):
+    self.a=a
+    self.b=b
+  def numbers(self):
+    array=[0]*(self.b-self.a)
+    iterator=0
+    start=self.a
+    if self.a==self.b:
+      array.append(self.a)
+      return array
+    elif self.a>self.b:
+      return None
+    array.append(self.count(array,start,self.b,iterator))
+    array.pop()
     return array
-  elif a>b:
-    return None
-  elif b<a:
-    return None
-  array.append(count(array,a,b,iterator))
-  array.pop()
-  return array
+  def count(self,array,a,b,iterator):
+    if a==self.b:
+      return array.append(self.b)
+    else:
+      array[iterator]=a
+    return self.count(array,a+1,self.b,iterator+1)
 
-def count(array,a,b,iterator):
-  if a==b:
-    array.append(b)
-  else:
-    array[iterator]=a
-    return count(array,a+1,b,iterator+1)
-
-
-"""def number(a,b):
-  if a>b:
-    return None
-  if a<0:
-    return None
-  print(a)
-  return number(a+1,b)"""
-
-def test_numbers():
+a=AandB(10,13)
+print(a.numbers())
+"""def test_numbers():
   z=0
   if numbers(4,10)==[4,5,6,7,8,9,10]:
     z+=1
@@ -40,4 +34,4 @@ def test_numbers():
   if numbers(3,2)==None:
     z+=1
   if z==4:
-    print("passed")
+    print("passed")"""
