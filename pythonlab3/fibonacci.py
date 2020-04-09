d7e7e0a47c41732c2b5ea5837d8eafcd97f62b23
h4mmilton <rmncsgo@gmail.com>
2020-04-09 19:35:44 +0600

    remove tests inside .py

--- pythonlab3/fibonacci.py
+++ pythonlab3/fibonacci.py
@@ -5,38 +5,13 @@ class fibonacci():
     self.b=1
     self.count=0
   def fibo(self):
+    if self.number==0:
+      return self.a
+    if self.number==1:
+      return self.b
     fib =self.a
     if self.count==self.number-2:
       return self.b+self.a
     self.a,self.b=self.b,self.a+self.b
     self.count+=1
     return self.fibo()
-
-a=fibonacci(42)
-print(a.fibo())
-
-
-  
-
-
-"""def test_fibo():
-  passed=0
-  if fibonacci(9)==34:
-    passed+=1
-  if fibonacci(10)==55:
-    passed+=1
-  if fibonacci(11)==89:
-    passed+=1
-  if fibonacci(12)==144:
-    passed+=1
-  if fibonacci(1)==1:
-    passed+=1
-  if fibonacci(2)==1:
-    passed+=1
-  if fibonacci(3)==2:
-    passed+=1
-  if passed==7:
-    print("passed")
-  else:
-    print("nope")
-"""
