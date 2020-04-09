3dc7c04ccae05fe74462c2c1baa9df14fe72584b
h4mmilton <rmncsgo@gmail.com>
2020-04-09 19:12:22 +0600

    reconstructed to class

--- pythonlab3/findmax.py
+++ pythonlab3/findmax.py
@@ -1,9 +1,17 @@
-def findmax(array,n):
-  if n==1:
-    return array[0]
-  return max(array[n-1],findmax(array,n-1))
+class array():
+    def __init__(self,array):
+        self.array=array
+    def prepare(self):
+        iterator=0
+        return self.findmax(iterator,self.array[0])
+    def findmax(self,iterator,number):
+        if iterator==len(self.array):
+            return number
+        elif self.array[iterator]>=number:
+            return self.findmax(iterator+1,self.array[iterator])
+        else:
+            return self.findmax(iterator+1,number)
 
 
-array=[1,4,45,6,200,10,3,4]
-n=len(array)
-print(findmax(array,n))
+a=array([23,100,33,4,2,30,45,23])
+print(a.prepare())
